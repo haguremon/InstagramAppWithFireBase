2d1e1cf5a35a34fdc3729312c36ff8ef2e68e457
Yuta iwasaki <yanyong0127@gmail.com>
2021-10-15 15:37:57 +0900

    SearchControllerのUIを作成

--- InstagramAppWithFireBase/Controller /SearchController.swift
+++ InstagramAppWithFireBase/Controller /SearchController.swift
@@ -10,14 +10,14 @@ import UIKit
 private let reuseIdentifier = "UserCell"
 private let cellIdentifer = "ProfileCell"
 
-class SearchController: UIViewController {
+class SearchController: UITableViewController {
 //
 //    // MARK: - Properties
-//    private let tableView = UITableView()
-//    private var users = [User]()
+  //  private let tableView = UITableView()
+    private var users = [User]()
 //    private var filteredUsers = [User]()
 //    private var posts = [Post]()
-//    private let searchController = UISearchController(searchResultsController: nil)
+//   private let searchController = UISearchController(searchResultsController: nil)
 //    private var inSearchMode: Bool{
 //        return searchController.isActive && !searchController.searchBar.text!.isEmpty
 //    }
@@ -37,10 +37,10 @@ class SearchController: UIViewController {
     override func viewDidLoad() {
 //        super.viewDidLoad()
 //        configureSearchController()
-//        configureUI()
+       configureUI()
 //        fetchUsers()
 //        fetchPosts()
-        view.backgroundColor = .darkGray
+        //view.backgroundColor = .darkGray
     }
 //
 //    // MARK: - API
@@ -60,24 +60,23 @@ class SearchController: UIViewController {
 //
 //    // MARK: - Helpers
 //
-//    func configureUI(){
-//        navigationItem.title = "Explore"
-//        view.backgroundColor = .white
-//        tableView.delegate = self
-//        tableView.dataSource = self
-//        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
-//        tableView.rowHeight = 64
-//
+   func configureUI(){
+        navigationItem.title = "Explore"
+        view.backgroundColor = .white
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
+       tableView.rowHeight = 64
 //        view.addSubview(tableView)
-//        tableView.fillSuperview()
+        tableView.fillSuperview()
 //        tableView.isHidden = true
 //
 //        view.addSubview(colletionView)
 //        colletionView.fillSuperview()
-//    }
+    }
 //
 //    func configureSearchController(){
-//        searchController.searchResultsUpdater = self
+//       // searchController.searchResultsUpdater = self
 //        searchController.obscuresBackgroundDuringPresentation = false
 //        searchController.hidesNavigationBarDuringPresentation = false
 //        searchController.searchBar.placeholder = "Search"
@@ -89,43 +88,44 @@ class SearchController: UIViewController {
 
 // MARK: - UITableViewDataSource
 //
-//extension SearchController: UITableViewDataSource{
-//     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return inSearchMode ? filteredUsers.count : users.count
-//    }
-//    
-//     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
+
+extension SearchController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return  10 //inSearchMode ? filteredUsers.count : users.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
 //        let user = inSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
 //        cell.viewModel = UserCellViewModel(user: user)
-//        return cell
-//    }
-//}
+         return cell
+    }
+}
 //
 //// MARK: - UITableViewDelegate
-//extension SearchController: UITableViewDelegate{
-//     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+extension SearchController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 //        let user = inSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
-//        let controller = ProfileController(user: user)
-//        navigationController?.pushViewController(controller, animated: true)
-//    }
-//}
+        //let controller = ProfileController(user: user)
+       // navigationController?.pushViewController(controller, animated: true)
+    }
+}
 //
 //// MARK: - UISearchBarDelegate
-//extension SearchController: UISearchBarDelegate{
-//    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-//        searchBar.showsCancelButton = true
-//        colletionView.isHidden = true
-//        tableView.isHidden = false
-//    }
-//    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-//        searchBar.endEditing(true)
-//        searchBar.showsCancelButton = false
-//        searchBar.text = nil
-//        colletionView.isHidden = false
-//        tableView.isHidden = true
-//    }
-//}
+extension SearchController: UISearchBarDelegate{
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = true
+       // colletionView.isHidden = true
+        tableView.isHidden = false
+    }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
+        searchBar.showsCancelButton = false
+        searchBar.text = nil
+       // colletionView.isHidden = false
+        tableView.isHidden = true
+    }
+}
 //
 //
 //// MARK: - UISearchResultsUpdating
---
+++ InstagramAppWithFireBase/Views/UserCell.swift
@@ -0,0 +1,81 @@
+//
+//  UserCell.swift
+//  InstagramClone
+//
+//  Created by IwasakI Yuta on 2021-10-15.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell{
+    
+    // MARK: - Properties
+//    var viewModel: UserCellViewModel? {
+//        didSet{
+//          configure()
+//        }
+//    }
+    
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        return iv
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "venom"
+        return label
+    }()
+    
+    private let fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "Test User"
+        label.textColor = .lightGray
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    //初期化された時に呼ばれる
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView)
+        //大きさ
+        profileImageView.setDimensions(height: 48  , width: 48)
+        profileImageView.layer.cornerRadius = 48 / 2
+        //centerY Y軸の位置を　セルの12スペースのところにprofileImageViewを配置
+        profileImageView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
+        
+        let stack = UIStackView(arrangedSubviews: [usernameLabel, fullnameLabel])
+        //縦の関係
+        stack.axis = .vertical
+        
+        stack.spacing = 4
+        //二つが左寄りに
+        stack.alignment = .leading
+        addSubview(stack)
+        //profileImageViewから右に８
+        stack.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 8)
+    
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(docer:) has not been implemented")
+    }
+    
+    // MARK: - Helpers
+    
+//    func configure(){
+//        guard let viewModel = viewModel else { return }
+//
+//        profileImageView.sd_setImage(with: viewModel.profileImageUrl)
+//        usernameLabel.text = viewModel.username
+//        fullnameLabel.text = viewModel.fullname
+//    }
+}
